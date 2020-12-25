a08f03da09b284aa0c30d794650cb18ea7b89fe6
ARGA APRILIAN PUTRA JAYA <arga.aprilian.ft18@mail.umy.ac.id>
2020-12-25 22:55:32 +0700

    Add project files.

---
+++ Client2_20180140099_ArgaAprilianPutraJaya/Form1.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client2_20180140103_Rama_Soni_Ravictor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxNama.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+            textBoxNIM.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[1].Value);
+            textBoxProdi.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[2].Value);
+            textBoxAngkatan.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[3].Value);
+        }
+
+        private void buttonJumlah_Click(object sender, EventArgs e)
+        {
+            var json = new WebClient().DownloadString("http://localhost:1909/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+            int length = data.Count();
+            labelJumlah.Text = Convert.ToString(length);
+        }
+        public class Mahasiswa
+        {
+            private string _nama, _nim, _prodi, _angkatan;
+            public string nama
+            {
+                get { return _nama; }
+                set { _nama = value; }
+            }
+
+            public string nim
+            {
+                get { return _nim; }
+                set { _nim = value; }
+            }
+
+            public string prodi
+            {
+                get { return _prodi; }
+                set { _prodi = value; }
+            }
+
+            public string angkatan
+            {
+                get { return _angkatan; }
+                set { _angkatan = value; }
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
